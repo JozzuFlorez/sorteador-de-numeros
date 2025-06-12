eda476068934e365a41258927291bee490fc829d
Josué Silva <d.silvaj999@gmail.com>
2025-06-12 11:31:10 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -6,6 +6,17 @@ function sortear() {
     let sorteados = [];
     let numero;
 
+    if( de > ate ){ 
+        alert(`o campo "DO NÚMERO" (onde você inseriu:${document.getElementById('de').value}) deve ser inferior ao campo "ATÉ O NÚMERO" (onde você inseriu:${document.getElementById('ate').value})`);
+        return;
+    } 
+
+    if (quantidade > (ate - de + 1)) {
+    alert('Campo "Quantidade" deve ser menor ou igual ao intervalo informado. Verifique!');
+    return;
+  }
+
+
     for (let i = 0; i < quantidade; i++) {
         numero = obterNumero(de, ate);
 
